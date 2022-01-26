30c34c1b5ee1ea683ae00b0f01ee973a2ff4bbe4
Karen Mejia <karen.mejia@softtek.com>
2022-01-26 17:53:37 -0600

    Mi primer commit

---
+++ src/components/journal/JournalScreen.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import { AuthRouter } from '../../routers/AuthRouter'
+
+export const JournalScreen = () => {
+    return(
+        <>
+            <h1>JournalScreen</h1>
+        </>
+    )
+} 
